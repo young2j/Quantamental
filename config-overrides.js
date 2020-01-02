3249b1116d5e0e3352c6ffc166d740286ecc7acd
ysj <173371929@qq.com>
2020-01-02 23:06:25 +0800

    开始构建项目

---
+++ config-overrides.js
@@ -0,0 +1,22 @@
+const {
+    override,
+    addDecoratorsLegacy,
+    addLessLoader,
+    fixBabelImports
+} = require('customize-cra')
+const modifyVars = require('./theme')
+
+
+
+module.exports = override(
+    fixBabelImports('import',{
+        libraryName:'antd',
+        libraryDirectory:'es',
+        style:true
+    }),
+    addLessLoader({
+        javascriptEnabled:true,
+        modifyVars
+    }),
+    addDecoratorsLegacy()
+)
